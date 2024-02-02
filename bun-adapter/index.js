ed3cc22482bb4d3bc9560690f13521bceca5d1a6
xxxhussein <huseeiin@outlook.com>
2024-02-02 14:40:19 +0300

    fix 3

--- bun-adapter/index.js
+++ bun-adapter/index.js
@@ -2,10 +2,12 @@ import { Server } from "SERVER";
 import { manifest } from "MANIFEST";
 
 const server = new Server(manifest);
-server.init({ env: process.env });
+server.init({ env: Bun.env });
 
 Bun.serve({
   fetch(req) {
-    return server.respond(req);
+    return server.respond(req, {
+      getClientAddress: () => this.requestIP(req)?.address,
+    });
   },
 });
